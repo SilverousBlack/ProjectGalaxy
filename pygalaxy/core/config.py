8c13754a62ec72b6aa44251fd85e137c11e330be
SilverousBlack <juliancalebsegundo@gmail.com>
2024-01-04 23:10:38 +0800

    added some tests, to fix `fixture`

--- pygalaxy/core/config.py
+++ pygalaxy/core/config.py
@@ -7,12 +7,13 @@ import jsonschema
 
 __module_init_flag = False
 
-COMMON_CONFIG: pl.Path
+COMMON_CONFIG_PATH: pl.Path
+COMMON_CONFIG: cp.ConfigParser
 GALAXY_CONFIG: dict
 
 def load(path: pl.Path):
-    global COMMON_CONFIG, GALAXY_CONFIG
-    COMMON_CONFIG_PATH: pl.Path = pl.Path(path).parents[1].joinpath("meta/.common.conf")
+    global COMMON_CONFIG_PATH, COMMON_CONFIG, GALAXY_CONFIG
+    COMMON_CONFIG_PATH =  pl.Path(path).parents[1].joinpath("meta/.common.conf")
     
     COMMON_CONFIG = cp.ConfigParser()
     COMMON_CONFIG.read(COMMON_CONFIG_PATH)
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,8 +1,21 @@
+import pytest
+
 class TestConfiguration:
     def test_default(self):
         from pygalaxy.core import config
         del config
         return
     
+    @pytest.fixture(scope="class")
+    def path_fixture(self, request):
+        return request.config.getoption("--path", default=None)
     
+    def test_config_reload(self, path_fixture):
+        if path_fixture is None:
+            return
+        else:
+            from pygalaxy.core import config
+            config.load(path_fixture)
+            print(path_fixture, config.COMMON_CONFIG_PATH)
+            assert config.COMMON_CONFIG_PATH == path_fixture
     
